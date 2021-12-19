b01ef4002e7395e0e379491be65718ae370e8034
phuongvy <phuongvy.maicogroup@gmail.com>
2021-12-19 10:00:22 +0700

    future and furetureBuilder widget example

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main()=> runApp(MyApp());
-
-//Demo 1 : Sử dụng Inherited Widget chia sẻ data
-//Demo 2 : Inherited Widget bên trong một stateful widget
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -20,102 +18,68 @@ class MyApp extends StatelessWidget {
           title : Text("Demo")
         ),
         body : Container(
-        child :  MyContainer(child:const Counter() )),
+        child : FutureWidget()
+        ),
 
       )
     );
   }
 }
 
-
-
-
-
-class DemoInheritedWidget extends InheritedWidget {
-  // int count =1000;
-
-  Widget child;
-  _MyContainerState? state ;
-  DemoInheritedWidget({ required this.child  ,  this.state, Key? key}) : super(key : key, child : child)  ;
+class FutureWidget extends StatefulWidget {
+  const FutureWidget({Key? key}) : super(key: key);
 
   @override
-  bool updateShouldNotify(DemoInheritedWidget old) {
-    return true;
-  }
+  _FutureWidgetState createState() => _FutureWidgetState();
 }
 
-class MyContainer extends StatefulWidget {
-  Widget child ;
-  MyContainer({required this.child, Key? key}) : super(key: key);
-
-  @override
-  _MyContainerState createState() => _MyContainerState();
-}
-
-class _MyContainerState extends State<MyContainer> {
-  int data = 0 ;
-   increment(){
-    // data++;
-    setState(() {
-
-    data++;
-    });
-  }
-
+class _FutureWidgetState extends State<FutureWidget> {
   @override
   Widget build(BuildContext context) {
-    return DemoInheritedWidget(child: widget.child, state: this);
-  }
-}
-
-class Counter extends StatefulWidget {
-  const Counter({Key? key}) : super(key: key);
-
-  @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  @override
-  Widget build(BuildContext context) {
-    DemoInheritedWidget? demo = context.dependOnInheritedWidgetOfExactType<DemoInheritedWidget>();
     return Center(
-        child: Container(
-            child :Column(
-                children : [
-                  Text(demo!.state!.data.toString()),
-                  TextButton(onPressed: ()=>demo.state!.increment(), child: Text("Incresement"))
-    ])));
+      child: Container(
+        child : Column(
+          children: [
+            FutureBuilder(
+              future: delayNumberError(),
+                builder:(contextt, snapshot){
+                if (snapshot.hasError)
+                {
+                  return Text(
+                    snapshot.error.toString()
+                  );
+                }
+                if(snapshot.hasData) {
+                  return Text(snapshot.data.toString());
+                }
+                return Text("Waiting for reload");
+                } ),
+            TextButton(
+                onPressed: (){
+             print("Demofurture");
+            }
+            , child: Text("Demo future"))
+          ],
+        )
+      )
+    );
   }
 }
 
 
+Future<void> asyncMethods(){
+  return Future.delayed(Duration(seconds: 2), ()=> print("asyncMethod"));
+}
 
+Future<int> asyncNumber(){
+  return Future.delayed(Duration(seconds: 2), ()=>200);
+}
+Future<int> asyncMethos(){
+  final c = Completer<int>();
+  Future.delayed(Duration(seconds: 2), ()=>c.complete(200));
+  return c.future;
+}
 
-//Demo 1
-//
-// class OngBa extends StatelessWidget {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(child: ChaMe());
-//   }
-// }
-//
-// class ChaMe extends StatelessWidget {
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(child: ConGai(),);
-//   }
-// }
-// class ConGai extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//   DemoInheritedWidget? demo = context.dependOnInheritedWidgetOfExactType<DemoInheritedWidget>();
-//     return Container(child : Text(demo!.count.toString()));
-//   }
-// }
-
-
+Future<int> delayNumberError(){
+  return Future.delayed(Duration(seconds: 2), ()=> throw Exception("Co loi xay ra"));
+}
